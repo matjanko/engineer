cd728a0e748664fc6986b333bbaa38ef849249e2
Mateusz Jankowski <mj@tek-projekt.pl>
2019-01-06 22:38:00 +0100

    Initial commit of helper layer

---
+++ Engineer.Utilities/MVVM Framework/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Engineer.Utilities
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {            
+            if (PropertyChanged != null)
+            {
+               PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void NotifyPropertyChanged<T>(Expression<Func<T>> exp)
+        {
+            MemberExpression memberExpression = (MemberExpression)exp.Body;
+            string propertyName = memberExpression.Member.Name;
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
